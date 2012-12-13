cec279218e865607e9a73586c0d70306fa5a94a9
Victor Borja <vborja@apache.org>
2012-12-12 21:11:15 -0600

    Save reports to mongodb
    using mongolab.com
    
      gdg_reporte/gdg_reporte0

---
+++ server/backend.js
@@ -0,0 +1,41 @@
+
+var mongoose = require('mongoose');
+var mongo = require('mongo');
+
+var dbUrl = "mongodb://gdg_reporte:gdg_reporte0@ds045507.mongolab.com:45507/gdg_reporte"
+
+var db = mongoose.connect(dbUrl, function(err) {
+    if (err) {
+        console.log("error in mongo connection");
+        throw err;
+    }
+    console.log("connected to mongo");
+});
+
+var Schema = mongoose.Schema;
+
+var Reporte = new Schema({
+  time: Number,
+  type: String,
+  image: {
+    path: String,
+    size: Number
+  },
+  location: [Number]
+});
+Reporte.index({ location: '2d' })
+
+var ReporteModel = db.model('Reporte', Reporte);
+
+var saveReport = function(report, cb){
+  var reporte = new ReporteModel(report)
+  reporte.save(cb);
+};
+
+exports.Reporte = ReporteModel;
+exports.db = db;
+exports.saveReport = saveReport;
+
+
+
+
