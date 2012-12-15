dd52a57bf82592b39cd82ffaeb1431256c2275eb
root <root@leon.(none)>
2012-12-15 15:59:06 -0600

    Agregando exif

--- src/mx/androidtitlan/GDGHackup/HomeActivity.java
+++ src/mx/androidtitlan/GDGHackup/HomeActivity.java
@@ -1,14 +1,21 @@
 package mx.androidtitlan.GDGHackup;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -20,6 +27,7 @@ public class HomeActivity extends Activity {
 	private final int flag = 1;
 	private ImageView imageView;
 	private Bitmap imagenCapturada;
+	private File file;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,32 +46,43 @@ public class HomeActivity extends Activity {
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_home, menu);
-		return true;
-	}
-
 	private void tomarFoto(int flag) {
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		this.file = new File(Environment.getExternalStorageDirectory(),
+				"imagen.jpg");
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+
 		startActivityForResult(intent, flag);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-		AlertDialog dialog;
+		super.onActivityResult(requestCode, resultCode, data);
+		AlertDialog dialog = null;
+		Uri uri = null;
 
 		if (resultCode == RESULT_CANCELED) {
 
 			dialog = crearDialogo("FOTO", "Fallo al tomar la foto", "OK");
+			dialog.show();
 
 		} else {
-			imagenCapturada = (Bitmap) data.getExtras().get("data");
-			dialog = crearDialogo("FOTO", "Éxito al tomar la foto", "OK");
+
+			uri = data.getData();
+			try {
+				imagenCapturada = Media.getBitmap(this.getContentResolver(),
+						uri);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// imagenCapturada = (Bitmap) data.getExtras().get("data");
+			dialog = crearDialogo("FOTO", "ÉXITO", "OK");
+			dialog.show();
 
 		}
-		dialog.show();
+
 	}
 
 	private AlertDialog crearDialogo(String title, String msg, String buttonText) {
@@ -76,6 +95,23 @@ public class HomeActivity extends Activity {
 			public void onClick(DialogInterface dialog, int idx) {
 				dialog.dismiss();
 				imageView.setImageBitmap(imagenCapturada);
+				try {
+					ExifInterface exifInterface = new ExifInterface(Environment
+							.getExternalStorageDirectory() + "/imagen.jpg");
+					Log.i("TAG_DATE", exifInterface
+							.getAttribute(ExifInterface.TAG_DATETIME));
+					Log.i("TAG_LATITUD",
+							""
+									+ exifInterface
+											.getAttribute(ExifInterface.TAG_GPS_LATITUDE));
+					Log.i("TAG_LONGITUD",
+							""
+									+ exifInterface
+											.getAttribute(ExifInterface.TAG_GPS_LONGITUDE));
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 
 			}
 		});
